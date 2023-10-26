30eec9a676afa7b6f2802584e69fe0f1ec8a9128
shinsuke.hirabayashi <shenfupinglin@gmail.com>
2023-10-26 06:55:55 +0000

    post

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['user_id'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
